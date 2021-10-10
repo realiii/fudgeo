7f40b2313aeb80df2b3bc8d1c1d1f466da6ab836
Jason Humber <jlhumber@openiii.com>
2021-10-10 09:35:55 -0500

    Add 3.9

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
