e8337439ede75b7890aa55e67c300f707766768a
Jason Humber <jlhumber@openiii.com>
2022-06-06 08:12:40 -0500

    #5 update setup.py -- fudgeo version and expanded python 3.x support (now 3.7 to 3.10)

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(join(dirname(__file__), 'README.md')) as readme:
 
 setup(
     name='fudgeo',
-    version='0.1.0',
+    version='0.1.1',
     author='Integrated Informatics Inc.',
     author_email='gis@integrated-informatics.com',
     description='GeoPackage support from Python.  fudgeo is a simple package '
@@ -26,7 +26,10 @@ setup(
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
