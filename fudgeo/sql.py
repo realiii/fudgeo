c5611a1e2b4f06dc54726424c3547c5b6b6bd64d
Jason Humber <jlhumber@openiii.com>
2023-05-20 18:08:57 -0500

    #48 whitespace

--- fudgeo/sql.py
+++ fudgeo/sql.py
@@ -224,8 +224,10 @@ DEFAULT_SRS_RECS: Tuple[Tuple[str, int, str, int, str, str], ...] = (
     ('Undefined Geographic SRS', 0, 'NONE', 0, 'undefined',
      'undefined geographic coordinate reference system'))
 
+
 EPSG_4326: str = """GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.01745329251994328,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]]"""
 ESRI_4326: str = """GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]]"""
+
 
 DEFAULT_EPSG_RECS: Tuple[Tuple[str, int, str, int, str, str], ...] = (
         DEFAULT_SRS_RECS + (('WGS 84', 4326, 'EPSG', 4326, EPSG_4326, ''),))
