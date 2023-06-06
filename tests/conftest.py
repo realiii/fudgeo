81ffe6fc78ab66e46ce40a3e2edd5ccffd9448b1
Jason Humber <jlhumber@openiii.com>
2023-06-05 21:12:28 -0300

    #61 testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -29,16 +29,17 @@ def setup_geopackage(tmp_path):
     Setup Basics
     """
     path = tmp_path.joinpath('test.gpkg')
-    gpkg = GeoPackage.create(path)
+    pkg = GeoPackage.create(path)
     srs = SpatialReferenceSystem(
         'WGS_1984_UTM_Zone_23N', 'EPSG', 32623, WGS_1984_UTM_Zone_23N)
+    pkg.add_spatial_reference(srs)
     fields = (
         Field('int.fld', SQLFieldType.integer),
         Field('text_fld', SQLFieldType.text),
         Field('test_fld_size', SQLFieldType.text, 100),
         Field('test_bool', SQLFieldType.boolean),
         Field('test_timestamp', SQLFieldType.timestamp))
-    yield path, gpkg, srs, fields
+    yield path, pkg, srs, fields
     if path.exists():
         path.unlink()
 # End setup_geopackage function
