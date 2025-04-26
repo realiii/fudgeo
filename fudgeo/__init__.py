d4726b6a3dc22229289e3bffa9aa0be186fb248d
Jason Humber <jlhumber@openiii.com>
2025-04-26 08:32:01 -0500

    #100 Update version to 1.0.1
    Bump version number from 1.0.0 to 1.0.1 in both __init__.py and pyproject.toml. This prepares the package for a new release with potential updates or fixes.

--- fudgeo/__init__.py
+++ fudgeo/__init__.py
@@ -9,7 +9,7 @@ from fudgeo.geopkg import (
     SpatialReferenceSystem, Field)
 
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 
 __all__ = ['GeoPackage', 'MemoryGeoPackage', 'Table', 'FeatureClass',
