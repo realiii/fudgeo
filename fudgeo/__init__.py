f6ea5f725ab3d402efdd628a3f28b1d3af9f426e
Jason Humber <jlhumber@openiii.com>
2025-10-26 16:18:25 -0500

    #110 Bump version to 1.3.0 in preparation for release.

--- fudgeo/__init__.py
+++ fudgeo/__init__.py
@@ -9,7 +9,7 @@ from fudgeo.geopkg import (
     SpatialReferenceSystem, Field)
 
 
-__version__ = '1.2.0'
+__version__ = '1.3.0'
 
 
 __all__ = ['GeoPackage', 'MemoryGeoPackage', 'Table', 'FeatureClass',
